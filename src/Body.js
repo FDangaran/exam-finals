cdb6dac05f25dbf171b1049fc73ee6c6341760dc
FDangaran <dangaranferds@gmail.com>
2024-02-01 23:48:14 +0800

    fixed responsiveness

--- src/Body.js
+++ src/Body.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faClock } from '@fortawesome/free-solid-svg-icons'
 import grab_generic from './IMAGES/grab_generic.png';
 import pizza from './IMAGES/pizza.png';
 import burger from './IMAGES/burger.png';
@@ -15,87 +14,87 @@ import SushiRoll from './IMAGES/SushiRoll.png';
 export default class Body extends Component{
 render() {
 return(
-<div className="promotion_parent">
-  <div className="deals">
-    <img className="deals" src={grab_generic} />
-    <h5>Deals up to</h5>
-    <h2 style={{color: '#00B14F'}}>30% OFF</h2>
+  <div className="promotion_parent">
+    <div className="deals">
+      <img className="deals" src={grab_generic} />
+      <h5>Deals up to</h5>
+      <h2 style={{color: '#00B14F'}}>30% OFF</h2>
+    </div>
+    <div className="exclusive">
+      <img className="exclusive" src={grab_generic} />
+      <h5>GrabFood</h5>
+      <h2 style={{color: 'var(--secondary-default, #FD6D22)'}}>Exclusive</h2>
+    </div>
+    <div className="bgcategory pizza">
+      <img src={pizza} />
+      <p>Pizza</p> 
+    </div>
+    <div className="bgcategory burger">
+      <img src={burger} />
+      <p>Burger</p> 
+    </div>
+    <div className="bgcategory bbq">
+      <img src={bbq} />
+      <p>BBQ</p> 
+    </div>
+    <div className="bgcategory sushi">
+      <img src={sushi} />
+      <p>Sushi</p> 
+    </div>
+    <div className="bgcategory vegan">
+      <img src={vegan} />
+      <p>Vegan</p> 
+    </div>
+    <div className="bgcategory desserts">
+      <img src={desserts} />
+      <p>Desserts</p> 
+    </div>
+    <div className="restaurants">
+      <h5>Nearby Restaurants</h5>
+    </div>
+    <div className="fdcategory">
+      <div className="Burgerking resto"> {/*gawa ng div class na iba para doon na lang yung same attributes ng bawat food DIBA DIBA*/}
+        <a href="https://food.grab.com/ph/en/restaurant/burger-king-welcome-rotonda-delivery/AWhefkWqR-bAtZoKZM96?">
+          <img src={BurgerKing} />
+          <h6>Burger King - UN Avenue Roxas</h6>
+          <i className="fa fa-clock-o" />
+          <p className="small-2">30-40 min ⋅ P79 min-sum</p>
+          <div className="burger_icon">
+            <img src={burger} />
+            <p>Burger</p>
+          </div>
+        </a></div><a href="https://food.grab.com/ph/en/restaurant/burger-king-welcome-rotonda-delivery/AWhefkWqR-bAtZoKZM96?">
+      </a>
+      <div className="snr resto"> {/*div class="fdcategory (food)"*/}
+        <a href="https://food.grab.com/ph/en/restaurant/s-r-new-york-style-pizza-lucky-chinatown-mall-available-for-long-distance-delivery-delivery/PHGFSTI000001d0?">
+          <img src={SnR} />
+          <h6>S&amp;R New York Style Pizza - SM</h6>
+          <i className="fa fa-clock-o" />
+          <p className="small-2">40-60 min ⋅ P626 min-sum</p>
+          <div className="burger_icon">
+            <img src={burger} />
+            <p>Burger</p>
+          </div>
+          <div className="pizza_icon">
+            <img src={pizza} />
+            <p>Pizza</p>
+          </div>
+        </a></div><a href="https://food.grab.com/ph/en/restaurant/s-r-new-york-style-pizza-lucky-chinatown-mall-available-for-long-distance-delivery-delivery/PHGFSTI000001d0?">
+      </a>
+      <div className="SushiRoll resto"> {/*gawa ng div class na iba para doon na lang yung same attributes ng bawat food DIBA DIBA*/}
+        <a href="https://food.grab.com/ph/en/restaurant/kanzen-sushi-roll-p-noval-street-available-for-long-distance-delivery-delivery/2-CZLJRYNXTP4EPA?">
+          <img src={SushiRoll} />
+          <h6>Kanzen Sushi Roll - P Noval Strt</h6>
+          <FontAwesomeIcon icon="faClock" />
+          <p className="small-2">20-40 min ⋅ P595 min-sum</p>
+          <div className="burger_icon">
+            <img src={sushi} />
+            <p>Sushi</p>
+          </div>
+        </a></div><a href="https://food.grab.com/ph/en/restaurant/kanzen-sushi-roll-p-noval-street-available-for-long-distance-delivery-delivery/2-CZLJRYNXTP4EPA?">
+      </a>
+    </div>
   </div>
-  <div className="exclusive">
-    <img className="exclusive" src={grab_generic} />
-    <h5>GrabFood</h5>
-    <h2 style={{color: 'var(--secondary-default, #FD6D22)'}}>Exclusive</h2>
-  </div>
-  <div className="bgcategory pizza">
-    <img src={pizza} />
-    <p>Pizza</p> 
-  </div>
-  <div className="bgcategory burger">
-    <img src={burger} />
-    <p>Burger</p> 
-  </div>
-  <div className="bgcategory bbq">
-    <img src={bbq} />
-    <p>BBQ</p> 
-  </div>
-  <div className="bgcategory sushi">
-    <img src={sushi} />
-    <p>Sushi</p> 
-  </div>
-  <div className="bgcategory vegan">
-    <img src={vegan} />
-    <p>Vegan</p> 
-  </div>
-  <div className="bgcategory desserts">
-    <img src={desserts} />
-    <p>Desserts</p> 
-  </div>
-  <div className="restaurants">
-    <h5>Nearby Restaurants</h5>
-  </div>
-  <div className="fdcategory">
-    <div className="Burgerking resto"> {/*gawa ng div class na iba para doon na lang yung same attributes ng bawat food DIBA DIBA*/}
-      <a href="https://food.grab.com/ph/en/restaurant/burger-king-welcome-rotonda-delivery/AWhefkWqR-bAtZoKZM96?">
-        <img src={BurgerKing} />
-        <h6>Burger King - UN Avenue Roxas</h6>
-        <i className="fa fa-clock-o" />
-        <p className="small-2">30-40 min ⋅ P79 min-sum</p>
-        <div className="burger_icon">
-          <img src={burger} />
-          <p>Burger</p>
-        </div>
-      </a></div><a href="https://food.grab.com/ph/en/restaurant/burger-king-welcome-rotonda-delivery/AWhefkWqR-bAtZoKZM96?">
-    </a>
-    <div className="snr resto"> {/*div class="fdcategory (food)"*/}
-      <a href="https://food.grab.com/ph/en/restaurant/s-r-new-york-style-pizza-lucky-chinatown-mall-available-for-long-distance-delivery-delivery/PHGFSTI000001d0?">
-        <img src={SnR} />
-        <h6>S&amp;R New York Style Pizza - SM</h6>
-        <i className="fa fa-clock-o" />
-        <p className="small-2">40-60 min ⋅ P626 min-sum</p>
-        <div className="burger_icon">
-          <img src={burger} />
-          <p>Burger</p>
-        </div>
-        <div className="pizza_icon">
-          <img src={pizza} />
-          <p>Pizza</p>
-        </div>
-      </a></div><a href="https://food.grab.com/ph/en/restaurant/s-r-new-york-style-pizza-lucky-chinatown-mall-available-for-long-distance-delivery-delivery/PHGFSTI000001d0?">
-    </a>
-    <div className="SushiRoll resto"> {/*gawa ng div class na iba para doon na lang yung same attributes ng bawat food DIBA DIBA*/}
-      <a href="https://food.grab.com/ph/en/restaurant/kanzen-sushi-roll-p-noval-street-available-for-long-distance-delivery-delivery/2-CZLJRYNXTP4EPA?">
-        <img src={SushiRoll} />
-        <h6>Kanzen Sushi Roll - P Noval Strt</h6>
-        <FontAwesomeIcon icon="faClock" />
-        <p className="small-2">20-40 min ⋅ P595 min-sum</p>
-        <div className="burger_icon">
-          <img src={sushi} />
-          <p>Sushi</p>
-        </div>
-      </a></div><a href="https://food.grab.com/ph/en/restaurant/kanzen-sushi-roll-p-noval-street-available-for-long-distance-delivery-delivery/2-CZLJRYNXTP4EPA?">
-    </a>
-  </div>
-</div>
 ) 
 }
 }
--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -7,22 +7,24 @@ import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
 export default class NavBar extends Component {
     render() {
         return (
-            <div className="d-inline justify-content-between">
-                <Navbar fixed="top" bg="light" variant="light">
-                    <Navbar.Brand href="#home">GrabFood</Navbar.Brand>
-                    <Form inline>
-                    <FormControl type="text" placeholder="Type your location" className="mr-sm-2" />
-                    </Form>
-                    <Nav className="ml-auto">
-                    <Nav.Link href="#bag">Bag</Nav.Link>
-                    <Nav.Link href="/Login">Login/Sign Up</Nav.Link>
-                    </Nav>
-                </Navbar>
-                <Form>
-                    <FormControl type="text" placeholder="Search for a dish or restaurant" className="mr-sm-2" />
-                </Form>
+            <div className="d-inline justify-content-between" id="navbar">
+<Navbar collapseOnSelect expand="md" fixed="top" bg="light" variant="light">
+    <Navbar.Brand href="#home">GrabFood</Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+            <Nav.Link href="#bag">Bag</Nav.Link>
+            <Nav.Link href="/Login">Logout</Nav.Link>
+        </Nav>
+        <Form inline>
+            <FormControl type="text" placeholder="Type your location" className="mr-sm-2" />
+        </Form>
+    </Navbar.Collapse>
+</Navbar>
+
             </div>
   );
 }
     }
 
+    
